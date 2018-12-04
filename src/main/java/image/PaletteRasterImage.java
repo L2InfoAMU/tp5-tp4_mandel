babb8d2499005b00c7c58238df9d863c8c0fd1f5
oniwas14 <johiko@hotmail.com>
2018-12-04 20:36:25 +0100

    Methode setWidth de la classe PaletteRasterImage terminee avec commentaire.

---
+++ src/main/java/image/PaletteRasterImage.java
@@ -0,0 +1,39 @@
+package image;
+
+import javafx.scene.paint.Color;
+import java.util.List;
+
+public class PaletteRasterImage implements Image {
+    int height;
+    int width;
+    int[][] indexesOfColors;
+    List<Color> palette;
+
+
+    /**
+     * Modifie la largeur de la fenetre.
+     * @param width
+     */
+    protected void setWidth(int width){
+        this.width = width;
+    }
+
+    protected void setHeight(int height){
+        this.height = height;
+    }
+
+    @Override
+    public Color getPixelColor(int x, int y) {
+        return this.;
+    }
+
+    @Override
+    public int getWidth() {
+        return this.width;
+    }
+
+    @Override
+    public int getHeight() {
+        return this.height;
+    }
+}
