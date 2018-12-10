b01488a3c53a6bee89b95b38f4c6ca6d8598e966
oniwas14 <johiko@hotmail.com>
2018-12-10 14:54:34 +0100

    Classe VectorImage commentee

--- src/main/java/image/VectorImage.java
+++ src/main/java/image/VectorImage.java
@@ -10,6 +10,12 @@ public class VectorImage implements Image{
     private int width;
     private List<Shape> shapes;
 
+    /**
+     * Constructeur de la classe VectorImage
+     * @param shapes de type List<Shape>.
+     * @param width de type entier.
+     * @param height de type entier.
+     */
     public VectorImage(List<Shape> shapes, int width, int height){
         setHeight(height);
         setWidth(width);
@@ -19,14 +25,28 @@ public class VectorImage implements Image{
         }
     }
 
+    /**
+     * Renvoi la largeur de la fenetre.
+     * @param height
+     */
     protected void setHeight(int height){
         this.height = height;
     }
 
+    /**
+     * Modifie la largeur de la fenetre.
+     * @param width
+     */
     protected void setWidth(int width){
         this.width = width;
     }
 
+    /**
+     * La methode retourne la couleur d’un pixel.
+     * @param x de type entier
+     * @param y de type entier
+     * @return la couleur contenue dans la forme
+     */
     @Override
     public Color getPixelColor(int x, int y) {
         Point toSearch = new Point(x, y);
@@ -38,11 +58,19 @@ public class VectorImage implements Image{
         return Color.WHITE;
     }
 
+    /**
+     * Renvoie la largeur de la fenetre.
+     * @return this.width
+     */
     @Override
     public int getWidth() {
         return this.width;
     }
 
+    /**
+     * Renvoie la hauteur de la fenetre.
+     * @return this.height
+     */
     @Override
     public int getHeight() {
         return this.height;
