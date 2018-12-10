df30fd4ebd056ef68d17a64ef744eabd12cc0ed3
oniwas14 <johiko@hotmail.com>
2018-12-10 11:34:11 +0100

    classe SparseRasterImage terminee

--- src/main/java/image/SparseRasterImage.java
+++ src/main/java/image/SparseRasterImage.java
@@ -10,9 +10,14 @@ import static util.Matrices.*;
 public class SparseRasterImage implements Image {
     private int width;
     private int height;
-    private Point point;
     private HashMap<Point, Color> pixelsMap;
 
+    /**
+     * Constructeur de l'objet.
+     * @param color de type Color
+     * @param width de type entier
+     * @param height de type entier
+     */
     public SparseRasterImage(Color color, int width, int height){
         setWidth(width);
         setHeight(height);
@@ -20,6 +25,10 @@ public class SparseRasterImage implements Image {
         setPixelsColor(color);
     }
 
+    /**
+     * Constructeur de l'objet SparseRasterImage.
+     * @param pixels
+     */
     public SparseRasterImage(Color[][] pixels){
         requiresRectangularMatrix(pixels);
         requiresNonNull(pixels);
@@ -30,6 +39,9 @@ public class SparseRasterImage implements Image {
         setPixelsColor(pixels);
     }
 
+    /**
+     * Cree le dictionnaire HashMap
+     */
     public void createRepresentation(){
         this.pixelsMap = new HashMap<>();
     }
@@ -50,20 +62,65 @@ public class SparseRasterImage implements Image {
         this.height = height;
     }
 
+    /**
+     * La methode associe le point de coordonnées x, y a la couleur
+     * @param color de type Color.
+     * @param x de type entier.
+     * @param y de type entier.
+     */
     public void setPixelColor(Color color, int x, int y){
         this.pixelsMap.put(new Point(x, y), color);
     }
 
-    @Override
-    public Color getPixelColor(int x, int y) {
-        return pixelsMap.get(new Point(x, y));
+    /**
+     * La methode change tous les pixels de la hashMap en la couleur passe en parametre
+     * @param color de type Color.
+     */
+    private void setPixelsColor(Color color){
+        for (int row = 0; row < getWidth(); row++){
+            for (int column = 0; column < getHeight(); column++){
+                setPixelColor(color, row, column);
+            }
+        }
     }
 
+    /**
+     * LA methode met à jour les valeurs de couleurs de l’image en utilisant les valeurs de la
+     * matrice donnée en paramètre.
+     * @param pixels une matrice de type Color.
+     */
+    private void setPixelsColor(Color[][] pixels){
+        for (int row = 0; row < getWidth(); row++){
+            for (int column = 0; column < getHeight(); column++){
+                setPixelColor(pixels[row][column], row, column);
+            }
+        }
+    }
+
+    /**
+     * La methode retourne la couleur d’un pixel.
+     * @param x de type entier.
+     * @param y de type entier.
+     * @return retourne la couleur d'un pixel ou blanc si aucune couleur.
+     */
+    @Override
+    public Color getPixelColor(int x, int y) {
+        return pixelsMap.getOrDefault(new Point(x, y), Color.WHITE);
+    }
+
+    /**
+     * Renvoie la largeur de la fenetre.
+     * @return this.width
+     */
     @Override
     public int getWidth() {
         return this.width;
     }
 
+    /**
+     * Renvoie la hauteur de la fenetre.
+     * @return this.height
+     */
     @Override
     public int getHeight() {
         return this.height;
--- src/main/java/viewer/Display.java
+++ src/main/java/viewer/Display.java
@@ -27,8 +27,10 @@ public class Display implements Initializable {
         // TODO : rajouter la création d'une fabrique d'image pour initialiser imageFactory
        // imageFactory = new RasterUniformImageFactory(200, 200, Color.RED, RasterImageType.BRUTE);
        //imageFactory = new RasterUniformImageFactory(200, 200, Color.RED, RasterImageType.PALETTE);
+        //imageFactory = new RasterUniformImageFactory(200, 200, Color.RED, RasterImageType.SPARSE);
        // imageFactory = new RasterFlagFactory(900, 600, Color.BLUE, Color.WHITE, Color.RED, RasterImageType.BRUTE);
-        imageFactory = new RasterFlagFactory(900, 600, Color.BLUE, Color.WHITE, Color.RED, RasterImageType.PALETTE);
+        //imageFactory = new RasterFlagFactory(900, 600, Color.BLUE, Color.WHITE, Color.RED, RasterImageType.PALETTE);
+        imageFactory = new RasterFlagFactory(900, 600, Color.BLUE, Color.WHITE, Color.RED, RasterImageType.SPARSE);
         this.image = imageFactory.makeImage();
 
         render();
